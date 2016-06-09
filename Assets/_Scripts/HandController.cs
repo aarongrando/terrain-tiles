7f3ab0b83c128e1a2f709855a4d8221daf7c9daa
Aaron Grando <aaron.grando@gmail.com>
2016-06-09 00:11:50 -0400

    game over screen

--- Assets/_Scripts/HandController.cs
+++ Assets/_Scripts/HandController.cs
@@ -1,8 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class HandController : MonoBehaviour {
+public class HandController : MonoBehaviour
+{
 
     public GameObject selected_card;
+    public GameObject game_over_text;
 
+    private GameObject[] cards;
+
+    void Start()
+    {
+        cards = GameObject.FindGameObjectsWithTag("HandCard");
+    }
+
+    void Update()
+    {
+        bool hand_full = false;
+        foreach (GameObject card in cards)
+        {
+            if (card.GetComponent<CardController>().card_value != null)
+            {
+                hand_full = true;
+                break;
+            }
+        }
+
+        if (!hand_full)
+        {
+            game_over_text.SetActive(true);
+        }
+    }
 }
